c253e952298e47600cf8cb24afed03a950d17185
farthead0912 <ambushfury2194@gmail.com>
2025-04-07 23:00:08 +0800

    Refactored JwtUtil to use @Component instead of @Configuration; removed unnecessary @Bean annotations.

--- backend/hms/src/main/java/edu/cit/hms/configuration/JwtUtil.java
+++ backend/hms/src/main/java/edu/cit/hms/configuration/JwtUtil.java
@@ -4,20 +4,18 @@ import java.util.Date;
 
 import javax.crypto.SecretKey;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 
-@Configuration
+@Component
 public class JwtUtil {
     private static final String SECRET_KEY = "jvnHumxyKgHSNbWb7SoFnWcW5nD0dGCx"; // testing purposes, this is 256 bits and encoded in HS256
     private static final long EXPIRATION_TIME = 86400000;
 
     private final SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 
-    @Bean
     public String generateToken(String username, String role, int userId) {
         return Jwts.builder()
                     .subject(username)
@@ -29,7 +27,6 @@ public class JwtUtil {
                     .compact();
     }
 
-    @Bean
     public Claims validateToken(String token) {
         return Jwts.parser()
                     .verifyWith(key)
